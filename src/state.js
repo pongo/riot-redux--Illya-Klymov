ef84d305f1d39c4be38b1fac43fee3ff28a4a69b
Pavel Shakhov <ab.pongo@gmail.com>
2016-07-22 22:00:26 +0300

    [11:26] чистая функция: не меняем объект, а возвращаем новый.

--- src/state.js
+++ src/state.js
@@ -1,12 +1,15 @@
 function reducer(state, action) {
   switch (action.type) {
     case 'INCREASE_COUNTER':
-      state.counter +=1;
-      break;
+      /*
+      Обе конструкции транспилируются в одинаковый код (пример: < http://bit.ly/2amoiCC >):
+      - {...state, ...{counter: (state.counter + 1)}}
+      - {...state, counter: (state.counter + 1)}
+      */
+      return {...state, ...{counter: (state.counter + 1)}};
     case 'RESET_COUNTER':
-      state.counter = 0;
-      break;
+      return {...state, ...{counter: 0}};
     default:
-      break;
+      return state;
   }
 }
